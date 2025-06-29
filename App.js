834fb294e7461987ed6e60d744fa7456d8bddf2a
unknown <veeresh.bezawada09@gmail.com>
2025-06-28 20:30:14 -0400

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import LoginPage from './pages/Login';
+import RegisterPage from './pages/Register';
+import HomePage from './pages/Home'
+import {createDrawerNavigator} from '@react-navigation/drawer'
+import { configureStore } from '@reduxjs/toolkit';
+import { useReducer } from 'react';
+import {Provider} from 'react-redux'
+import { userReducer } from './state/user';
+import { NavigationContainer } from '@react-navigation/native';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
-export default function App() {
+
+const Tab = createDrawerNavigator()
+
+const store = configureStore({
+  reducer: {
+    user: userReducer
+  }
+})
+
+function AuthDrawer() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomePage} options={{ headerShown: false}} />
+      <Tab.Screen name="Register" component={RegisterPage} options={{ headerShown: false}} />
+      <Tab.Screen name="LogIn" component={LoginPage} options={{ headerShown: false}}  />
+    </Tab.Navigator>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+export default function App() {
+  let fontsLoaded = useFonts({
+        Roboto_400Regular,
+        Roboto_700Bold,
 });
+  return (
+    <Provider store={store} style={{fontFamily: 'Roboto_400Regular'}}>
+      <NavigationContainer>
+      <AuthDrawer />
+      </NavigationContainer>
+    </Provider>
+  );
+}
